b7cc30c55a75ffd8b7330288382a900a49b7ba08
samuel-mferreira <samuel.mferreira@outlook.com>
2022-04-24 23:32:23 -0300

    login page html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'authentication/login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'authentication',
+        loadChildren: () => import('./modules/authentication/authentication.module').then(m => m.AuthenticationModule)
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/modules/authentication/pages/login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+    projectName: string;
+    logo!: string;
+    constructor() {
+        this.projectName = 'Project Name'
+    }
+
+    ngOnInit(): void {
+    }
+
+}
